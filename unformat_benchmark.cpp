d29764c3a5ed31a95d6ecb00bd13539ccc6f0968
Adam Yaxley <craincat666@gmail.com>
2017-12-14 23:13:57 +0900

    Integrate Google Benchmark

---
+++ unformat_benchmark.cpp
@@ -0,0 +1,74 @@
+#include <benchmark/benchmark.h>
+#include <regex>
+#include "unformat.h"
+
+const std::string g_input = "Harry is 18 years old and weighs 67.8 kilograms";
+
+static void Unformat(benchmark::State& state)
+{
+	std::string name;
+	int age;
+	float weight;
+	std::string units;
+	for (auto _ : state)
+	{
+		ay::unformat(g_input, "{} is {} years old and weighs {} {}", name, age, weight, units);
+		benchmark::DoNotOptimize(name);
+		benchmark::DoNotOptimize(age);
+		benchmark::DoNotOptimize(weight);
+		benchmark::DoNotOptimize(units);
+	}
+}
+
+BENCHMARK(Unformat);
+
+static void StringStream(benchmark::State& state)
+{
+	std::string name;
+	int age;
+	float weight;
+	std::string units;
+	for (auto _ : state)
+	{
+		std::istringstream stream(g_input);
+		stream >> name;
+		stream.ignore(2);
+		stream >> age;
+		stream.ignore(21);
+		stream >> weight;
+		stream >> units;
+		benchmark::DoNotOptimize(name);
+		benchmark::DoNotOptimize(age);
+		benchmark::DoNotOptimize(weight);
+		benchmark::DoNotOptimize(units);
+	}
+}
+
+BENCHMARK(StringStream);
+
+static void StdRegex(benchmark::State& state)
+{
+	std::string name;
+	int age;
+	float weight;
+	std::string units;
+	for (auto _ : state)
+	{
+		std::regex regex("([A-Za-z]+) is ([0-9]+) years old and weighs ([0-9\.]+) ([A-Za-z]+)");
+		std::smatch matches;
+		std::regex_search(g_input, matches, regex);
+
+		name = matches[1];
+		age = std::stoi(matches[2]);
+		weight = std::stof(matches[3]);
+		units = matches[4];
+		benchmark::DoNotOptimize(name);
+		benchmark::DoNotOptimize(age);
+		benchmark::DoNotOptimize(weight);
+		benchmark::DoNotOptimize(units);
+	}
+}
+
+BENCHMARK(StdRegex);
+
+BENCHMARK_MAIN();
